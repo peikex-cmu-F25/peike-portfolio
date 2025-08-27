52d8b0177f6a0908fe73ad2a8cc381bda6645e0b
peikex-cmu-F25 <peikex@andrew.cmu.edu>
2025-08-27 14:52:27 -0700

    cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
       rehypePlugins: [rehypeSlug, rehypeKatex, rehypeHighlight],
     }),
   ],
-  base: '/peike-portfolio/',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
